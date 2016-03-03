c4cbab715441df1a62c88602c9d577f1b6a2c1bc
gitforschool <edrick@ualberta.ca>
2016-03-02 17:20:08 -0700

    solved audio issue with distortion

--- distortion_component.vhd
+++ distortion_component.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity distort is
 	port( 
@@ -16,15 +17,28 @@ entity distort is
 end entity distort;
 
 architecture behavior of distort is
-constant clipping_default : std_logic_vector(15 downto 0) := "0000001111101000"; -- constant value of 500 in decimal
+
+-- Distortion Settings
+-- 1) Gain at 3 ("11") - Clipping at 1000 "0000001111101000"
+-- 2) Gain at 3 ("10") - Clipping at 3000 "0000101110111000" - Default
+-- 3) Gain at 3 ("01") - Clipping at 5000 "0001001110001000"
+
+constant clipping_default : std_logic_vector(15 downto 0) := "0000101110111000"; -- constant value of 3000 in decimal
+constant clipping_low : std_logic_vector(15 downto 0) := "0001001110001000"; -- constant value of 1000 in decimal
+constant clipping_high : std_logic_vector(15 downto 0) := "0000001111101000"; -- constant value of 5000 in decimal
+constant gain_constant : std_logic_vector(1 downto 0) := "10"; -- constant gain
 signal clip_threshold : std_logic_vector(15 downto 0);
 signal completed : std_logic :='0';
 
+
 begin
+
+	
 	done <= completed;
 	g0:process(clk,reset,dist_en,ready)
+	variable mult_result : std_logic_vector(17 downto 0);
 	begin
-	-- clip_threshold <= clipping_value;
+
 	-- Default Clipping Value
 	clip_threshold <= clipping_default; 
 		if reset = '0' then
@@ -32,18 +46,35 @@ begin
 		--elsif (clk='1' and clk'event) then
 		elsif (rising_edge(clk)) then
 			if(ready = '1') then
+				-- Clipping Configurations --
+				--if(clipping_value = "0001001110001000") then
+				--	clip_threshold <= "0001001110001000";
+				--elsif(clipping_value = "0000001111101000") then
+				--	clip_threshold <= "0000001111101000";
+				--else
+				--	clip_threshold <= "0000101110111000";
+				--end if;
+				-- End Clipping Configurations
 				if dist_en = '1' then -- Check if Distortion is Enabled
 					if data_in(15) = '1' then -- Check sign of sample (If negative...)
 						if (not data_in(14 downto 0)) >= clip_threshold(14 downto 0) then -- compare data to clip_threshold (without sign bits)
-							data_out <= '1' & (not clip_threshold(14 downto 0)); -- if data is greater than threshold, data_out = clip_threshold, concatenate '1' to complement
+							mult_result := gain_constant * clip_threshold;
+							data_out <= '1' & (not mult_result(14 downto 0));
+							--data_out <= '1' & (not clip_threshold(14 downto 0)); -- if data is greater than threshold, data_out = clip_threshold, concatenate '1' to complement
 						else 
-							data_out <= data_in;
+							mult_result := gain_constant * data_in;
+							data_out <= mult_result(15 downto 0);
+							--data_out <= data_in;
 						end if;
 					elsif data_in(15) = '0' then -- Check sign of sample (If positive...)
 						if data_in(14 downto 0) >= clip_threshold(14 downto 0) then
-							data_out <= '0' & clip_threshold(14 downto 0);
+							mult_result := gain_constant * clip_threshold;
+							data_out <= '0' & mult_result(14 downto 0);
+							--data_out <= '0' & clip_threshold(14 downto 0);
 						else
-							data_out <= data_in;
+							mult_result := gain_constant * data_in;
+							data_out <= mult_result(15 downto 0);
+							--data_out <= data_in;
 						end if;
 					end if;
 				else
